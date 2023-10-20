bc579d5d52da32b3d887f4d83f0233c5eeb89dda
James Thompson <closetsolipsist@gmail.com>
2023-10-19 22:49:07 -0400

    Added initial version of CLI * MigrationRecordkeeper class added to keep track of open migration   in laridae.open_migration table. * MigrationExecutor restructured to perform expand/contract/rollback   separately: it now parses command line arguments, stores the DB url in   a file, and call MigrationRecordkeeper methods. * laridae CLI added to root folder; it initializes a MigrationExecutor   and passes it the command line arguments it received. * Sample migration.json script added to root folder to ease trying   out CLI. * README updated.

--- components/MigrationExecutor.rb
+++ components/MigrationExecutor.rb
@@ -1,3 +1,5 @@
+require_relative './DatabaseConnection'
+require_relative './MigrationRecordkeeper'
 require_relative '../operations/AddNotNull'
 require_relative '../operations/RenameColumn'
 require_relative '../operations/AddCheckConstraint'
@@ -6,6 +8,7 @@ require_relative '../operations/CreateIndex'
 require 'json'
 
 class MigrationExecutor
+  DB_URL_FILENAME = "#{__dir__}/.laridae_database_url.txt"
   HANDLERS_BY_OPERATION = {
     "add_not_null" => AddNotNull,
     'rename_column' => RenameColumn,
@@ -13,32 +16,118 @@ class MigrationExecutor
     'drop_column' => DropColumn,
     'create_index' => CreateIndex
   }
-  def initialize(db_connection, migration_script)
-    @database = db_connection
-    @database.turn_off_notices
-    @script = JSON.parse(migration_script)
+  def initialize
+    @database = nil
   end
 
-  def run
-    operation_name = @script["operation"]
-    info = @script["info"]
-    schema = info["schema"]
-    table = info["table"]
-    operation_handler = HANDLERS_BY_OPERATION[operation_name].new(@database, @script)
-    puts "Should clean up be done on the database? (Y/N) "
-    choice = gets.chomp.upcase
-    operation_handler.rollback if choice == 'Y' 
- 
-    operation_handler.expand
-    
-    puts "Is it safe to contract? (Y/N) "
-    choice = gets.chomp.upcase
-    if choice == 'Y' 
-      operation_handler.contract 
-      puts 'Expand and Contract completed'
-    else 
-      puts 'Expand only'
+  def run_command(command_line_arguments)
+    arguments_per_command = {"init" => 2, "expand" => 2, "contract" => 1, "rollback" => 1}
+    command = command_line_arguments[0]
+    if !arguments_per_command.key?(command)
+      puts "Invalid command."
+      return
     end
+    if arguments_per_command[command] > command_line_arguments.length
+      puts "Missing required argument."
+      return
+    elsif arguments_per_command[command] < command_line_arguments.length
+      puts "Extra arguments supplied."
+      return
+    end
+    send(*command_line_arguments)
+  end
+
+  def database_connection_from_file
+    # add handling for if file does not exist
+    # todo: close files
+    database_url = File.open(DB_URL_FILENAME).read
+    database = DatabaseConnection.new(database_url)
+    database.turn_off_notices
+    database
+  end
+
+  def new_schema_search_path
+    db_url = File.open(DB_URL_FILENAME).read
+    if db_url.include?("?")
+      "#{db_url}&currentSchema=laridae_after,public"
+    else
+      "#{db_url}?currentSchema=laridae_after,public"
+    end
+  end
+
+  # not to be confused with initialize; handles init command
+  def init(db_url)
+    # todo: handle this situation better
+    if File.exists?(DB_URL_FILENAME)
+      puts "Note: previously initialized; overwriting."
+    end
+    db_url_file = File.open(DB_URL_FILENAME, 'w')
+    db_url_file.write(db_url)
+    db_url_file.close
+    # todo: add error handling if URL is invalid
+    @database = DatabaseConnection.new(db_url)
+    MigrationRecordkeeper.new(@database).create_open_migration_table
     @database.close
+  end
+
+  def operation_handler_for_script(script)
+    operation_name = script["operation"]
+    HANDLERS_BY_OPERATION[operation_name].new(@database, script)
+  end
+
+  def expand(filename)
+    @database = database_connection_from_file
+    migration_recordkeeper = MigrationRecordkeeper.new(@database)
+    if !filename.end_with?(".json")
+      puts "Migration script #{filename} must be JSON."
+      return
+    end
+    begin
+      migration_script_file = File.open(filename)
+    rescue Errno::ENOENT
+      puts "Migration script \"#{filename}\" not found."
+      return
+    end
+    if migration_recordkeeper.open_migration?
+      puts "Another migration is currently running; cannot continue."
+      return
+    end
+    migration_script = migration_script_file.read
+    script_hash = JSON.parse(migration_script)
+    operation_handler = operation_handler_for_script(script_hash)
+    puts "Should clean up be done on the database (Y/N)"
+    choice = STDIN.gets.chomp.upcase
+    operation_handler.rollback if choice == 'Y' 
+    operation_handler.expand
+    migration_recordkeeper.record_new_migration(migration_script)
+    puts "Expand complete: new schema available at #{new_schema_search_path}"
+  end
+
+  def contract
+    @database = database_connection_from_file
+    migration_recordkeeper = MigrationRecordkeeper.new(@database)
+    if !migration_recordkeeper.open_migration?
+      puts "No open migration; cannot contract."
+    end
+    migration_script = migration_recordkeeper.open_migration
+    script_hash = JSON.parse(migration_script)
+    operation_handler = operation_handler_for_script(script_hash)
+    operation_handler.contract
+    migration_recordkeeper.remove_current_migration
+    puts "Contract complete"
+  end
+
+  def rollback
+    @database = database_connection_from_file
+    migration_recordkeeper = MigrationRecordkeeper.new(@database)
+    if !migration_recordkeeper.open_migration?
+      puts "No open migration; cannot rollback."
+    end
+    migration_script = migration_recordkeeper.open_migration
+    script_hash = JSON.parse(migration_script)
+    operation_handler = operation_handler_for_script(script_hash)
+    operation_handler.rollback
+    migration_recordkeeper.remove_current_migration
+    puts "Rollback complete"
   end
 end
