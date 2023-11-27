a52e1ced2970bc6f7f28d26c0cd63a4b41509e11
Mai Khuu <66891232+MaiKhuu@users.noreply.github.com>
2023-11-27 14:06:00 -0500

    ascii graphic

--- components/CommandLineInterface.rb
+++ components/CommandLineInterface.rb
@@ -6,7 +6,7 @@ require_relative './DatabaseConnection'
 require_relative './Validator'
 require 'json'
 
-# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/MethodLength, Metrics/ClassLength
 class CommandLineInterface
   ARGUMENTS_PER_COMMAND = {
     'init' => 2,
@@ -36,8 +36,11 @@ class CommandLineInterface
     puts 'Command cannot be executed.'
   end
 
+  # MAIN COMMANDS
+  # =================================================
   def init(_)
     db_conn = DatabaseConnection.new(@db_url)
+    puts welcome_ascii
     MigrationRecord.new(db_conn).initialize_laridae
     puts 'Initialization successful.'
   rescue PG::Error => e
@@ -57,6 +60,7 @@ class CommandLineInterface
     Migration.new(db_conn, record, migration_script_json).expand
     puts "New schema can be accessed using the search_path: #{new_schema_search_path(migration_script_json['name'])}"
   rescue StandardError => e
+    db_conn.query_lockable('ROLLBACK;')
     puts "Error occured: #{e.message}"
     puts 'Expand terminated.'
   ensure
@@ -70,6 +74,7 @@ class CommandLineInterface
 
     Migration.new(db_conn, record, record.last_migration['script']).contract
   rescue StandardError => e
+    db_conn.query_lockable('ROLLBACK;')
     puts "Error occured: #{e.message}"
     puts 'Contract terminated.'
   ensure
@@ -79,6 +84,8 @@ class CommandLineInterface
   def rollback(_)
     db_conn = DatabaseConnection.new(@db_url)
     record = MigrationRecord.new(db_conn)
+    raise 'There is no active migration to rollback' unless record.ok_to_rollback?
+
     Migration.new(db_conn, record, record.last_migration['script']).rollback
   rescue StandardError => e
     puts "Error occured: #{e.message}"
@@ -93,13 +100,14 @@ class CommandLineInterface
     raise 'There is no migration eligible to restore' unless record.last_migration_aborted?
 
     Migration.new(db_conn, record, record.last_migration['script']).restore
-    # rescue StandardError => e
+  rescue StandardError => e
     puts "Error occured: #{e.message}"
     puts 'Restore terminated.'
   ensure
     db_conn&.close
   end
 
+  #===========================================================
   def validate_script(db_conn, migration_file_location)
     validation_result = Validator.run_with_location(db_conn, migration_file_location)
     raise "#{validation_result['message']}." unless validation_result['valid']
@@ -114,5 +122,30 @@ class CommandLineInterface
       "#{@db_url}?options=-csearch_path%3Dlaridae_#{migration_name},public"
     end
   end
+
+  def welcome_ascii
+    <<~ASCII
+
+                        /(((((((
+                      //((((((
+                    /////(((
+                ////////
+      %%%%%%%%    ////////
+        %%%%%%%%    ////////
+          %%%%%%%%    ////////
+              %%%%%%%%   ////////
+            &&%%%%%%
+          &&&&&%%%
+        &&&&&&&%
+      &&&&&&&%
+
+       _            _     _
+      | | __ _ _ __(_) __| | __ _  ___
+      | |/ _` | '__| |/ _` |/ _` |/ _ \\
+      | | (_| | |  | | (_| | (_| |  __\/
+      |_|\\__,_|_|  |_|\\__,_|\\__,_|\\___|
+
+    ASCII
+  end
 end
-# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/MethodLength, Metrics/ClassLength
